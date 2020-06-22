c44ebf4dcb34fdeec40437ee7875e4f9bab9de8b
Tesmic Jeffin <58257845+Tesmic@users.noreply.github.com>
2020-06-22 17:28:52 +0530

    Update settings.py
    DATABASES['default']

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['rapid-api-practicetj.herokuapp.com']
 # Heroku: Update database configuration from $DATABASE_URL. 
 import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Application definition
 
